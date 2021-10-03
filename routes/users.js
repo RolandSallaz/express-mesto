e5ed640913c60ed3232e7c810725176d9b8bd771
RolandSallaz <machodin@yandex.ru>
2021-10-03 17:29:44 +0300

    hotfix: eslint

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
-const { getUsers, getUser, createUser, updateUser, updateUserAvatar } = require('../controllers/users');
+const {
+  getUsers, getUser, createUser, updateUser, updateUserAvatar,
+} = require('../controllers/users');
 
 router.get('/', getUsers);
-router.get('/:id', getUser);
+router.get(
+  '/:id', getUser,
+);
 router.post('/', createUser);
 router.patch('/me', updateUser);
 router.patch('/me/avatar', updateUserAvatar);
