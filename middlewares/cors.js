39614c67007590dd3f2501961500d1f584db5fa1
RolandSallaz <machodin@yandex.ru>
2021-10-29 23:37:38 +0300

    hotfix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -11,6 +11,7 @@ module.exports = (req, res, next) => {
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
+  res.header('Access-Control-Allow-Origin', '*');
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', requestHeaders);
   }
